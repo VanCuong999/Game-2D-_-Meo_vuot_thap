443e20148549aa437407306528b266c91a8177ea
VanCuong999 <cuongnv3999@gmail.com>
2024-11-21 21:25:21 +0700

    phác thảo giao diện (Thành Tựu) trên Unity
    Hoàn thành phác thảo giao diện (Thành Tựu) trên Unity

--- Assets/showbanner.cs
+++ Assets/showbanner.cs
@@ -1,15 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class showbanner : MonoBehaviour
 {
     public GameObject banner1; // Kéo thả Banner1 vào đây
     public GameObject banner2; // Kéo thả Banner2 vào đây
-    
-    private int coin = 0; 
+
+    public TextMeshProUGUI textcoin;
+    public TextMeshProUGUI textHuyHieu;
+    private int coin = 0;
     private int huyhieu = 0;
 
+    private int cointext_gitf;
+    private int huyhieu_gitf;
+
+
+    private void Start() 
+    {
+        cointext_gitf = Random.Range(150, 300);
+        huyhieu_gitf = Random.Range(150, 300);
+
+        textcoin.text = "+"+ cointext_gitf;
+        textHuyHieu.text = "+"+ huyhieu_gitf;
+    }
     public void ShowRandomBanner()
     {
         // Ẩn cả hai banner
@@ -36,16 +51,16 @@ public class showbanner : MonoBehaviour
 
     public void btnNhanCoin()
     {
-        int cointext_gitf = Random.Range(150 ,300); 
+
         coin += cointext_gitf;
-        Debug.Log("Bạn đã nhận "+ cointext_gitf+" coin! Tổng số coin: " + coin);
+        Debug.Log("Bạn đã nhận " + cointext_gitf + " coin! Tổng số coin: " + coin);
         HideBanners();
     }
     public void btnNhanHuyHieu()
     {
-        int huyhieu_gitf = Random.Range(150 ,300); 
+
         huyhieu += huyhieu_gitf;
-        Debug.Log("Bạn đã nhận "+ huyhieu_gitf+" huy hieu! Tổng số huy hieu: " + coin);
+        Debug.Log("Bạn đã nhận " + huyhieu_gitf + " huy hieu! Tổng số huy hieu: " + huyhieu);
         HideBanners();
     }
 }
