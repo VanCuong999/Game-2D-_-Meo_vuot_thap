09e220d2d79576b079e4695a6e4956cabfae164a
VanCuong999 <cuongnv3999@gmail.com>
2024-11-23 21:21:55 +0700

    cập nhật Player Level 1
    a

---
+++ Assets/Script Player/HeathPlayer.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeathPlayer : MonoBehaviour
+{
+    public static HeathPlayer Intance;
+    public float minHeath;
+    public float maxHeath;
+
+    private float Heath;
+
+    [SerializeField] private Image heathImage;
+    [SerializeField] private TextMeshProUGUI heathText;
+    private void Awake() 
+    {
+        Intance = this;    
+    }
+    private void Start() 
+    {
+        Heath = minHeath;
+        UPdateHeath(Heath,maxHeath);    
+    }
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeHeath(10);
+        }
+        heathImage.fillAmount = Mathf.Lerp(heathImage.fillAmount,minHeath/maxHeath,10 * Time.deltaTime);
+        heathText.text = $"{minHeath} / {maxHeath}";
+    }
+
+    public void TakeHeath(float heath)
+    {
+        Heath -= heath;
+        UPdateHeath(Heath,maxHeath);
+
+        if (minHeath <= 0)
+        {
+            Character.Intance.KichHoatDie();
+        }
+    }
+
+    public void HoiMau(float heath)
+    {
+        Heath+= heath;
+        UPdateHeath(Heath,maxHeath);
+        if (Heath > maxHeath)
+        {
+            Heath = maxHeath;
+        }
+    }
+    public void UPLevelHeath()
+    {
+        minHeath += 30;
+        maxHeath += 30;
+    }
+
+    public void UPdateHeath(float minHeaths, float maxHeaths)
+    {
+        minHeath = minHeaths;
+        maxHeath = maxHeaths;
+    }
+
+}
