b153f0920b04835de46056036b2fe65ac49c45ba
ManhToan2612 <147154654+ManhToan2612@users.noreply.github.com>
2024-12-05 19:51:45 +0700

    Update SwordProjectile.cs

--- Assets/Script Player/SwordProjectile.cs
+++ Assets/Script Player/SwordProjectile.cs
@@ -33,10 +33,6 @@ public class SwordProjectile : MonoBehaviour
         {
             GolemEnemy.Intance.TakeDangage(40);
         }
-        if (collision.CompareTag("enemy3"))
-        {
-            enemy3tancong.Intance.TakeDamage(40);
-        }
     }
    
 
